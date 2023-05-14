7386fe5f16dacbeb72c68a53f45ad423a2afdd62
renamedu <roberto.k306@gmail.com>
2023-05-14 09:50:57 +0300

    2nd

--- index.php
+++ index.php
@@ -31,14 +31,14 @@ $products = mysqli_fetch_all($goods);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Shop</title>
+    <title>Test_web page</title>
     <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
     <div class="main">
         <h1 class="main-header">
-            Test_web shop
+            Test_web page
         </h1>
         <div class="main-content">
             <div class="main-content-sort">
